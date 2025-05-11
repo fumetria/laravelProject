bf5d7b255d5dfd4cfd73d308f4bbe5162b8149d9
fumetria <fulin789@gmail.com>
2025-05-11 16:31:56 +0200

    Add author of the month component for welcome page

---
+++ resources/js/Components/AuthorOfTheMonth.vue
@@ -0,0 +1,32 @@
+<script setup>
+import PrimaryButton from './PrimaryButton.vue';
+
+defineProps({
+    author: Object
+})
+
+</script>
+
+<template>
+    <div class="bg-stone-300">
+        <h2 class="capitalize">Autor del mes</h2>
+        <section>
+            <aside>
+                <img src="" alt="">
+            </aside>
+            <div>
+                <h2 class="uppercase">{{ author.name }}</h2>
+                <h2>Biografía</h2>
+            </div>
+        </section>
+        <section>
+            <h3>Bibliografía</h3>
+        </section>
+        <section>
+            <p>Encuentra sus libros en nuestro catálogo en el siguiente enlace</p>
+            <PrimaryButton>
+                
+            </PrimaryButton>
+        </section>
+    </div>
+</template>
