cb8fa869715e1bcbff9339c815085ba88a816d93
Fumetria <fulin789@gmail.com>
2025-05-19 12:39:21 +0200

    Add WelcomeHeader.vue

---
+++ resources/js/Components/WelcomeHeader.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+    <section class="">
+        <img src="/img/welcome/header.png" alt="" width="100%" height="400px" class="rounded-lg">
+    </section>
+</template>
