b4e9527084578324ad96e73cd525f20eb135b471
Fumetria <fulin789@gmail.com>
2025-05-19 22:12:30 +0200

    Add new images for Welcome section and update FeatureSection layout. Fix error with colors in buttons

--- resources/js/Components/FeatureSection.vue
+++ resources/js/Components/FeatureSection.vue
@@ -6,7 +6,7 @@
 </script>
 
 <template>
-    <div class="flex flex-col bg-slate-100 justify-center items-center w-full max-w-4xl mx-auto my-5 p-5 shadow">
+    <div class="flex flex-col bg-slate-100 justify-center items-center w-full max-w-4xl mx-auto my-0 px-6 py-7 shadow">
         <div class="flex justify-start text-left font-light text-2xl">
             <slot name="title" />
         </div>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -24,60 +24,65 @@ const welcomeDescription = 'Accede a nuestro catálogo y encuentra el libro que 
         <section class="bg-slate-200">
             <WelcomeHeader :welcome-title="welcomeTitle" :welcome-description="welcomeDescription"></WelcomeHeader>
             <AuthorOfTheMonth :author="author"></AuthorOfTheMonth>
-            <section class="py-5">
-                <FeatureSection>
-                    <template #title>
-                        <h2>Nuestro catálogo</h2>
-                    </template>
-                    <template #content class="">
-                        <img src="/img/welcome/catalog-img.webp" alt="Imagen de una biblioteca" width="400px"
-                            height="300px" class="rounded-lg float-right ml-4 mb-4">
-                        <p>Disponemos de un amplio catálogo de libros. Accede a nuestro catálogo y encuentra el libro
-                            que necesitas para tu trabajo o simplemente algo para leer</p>
+            <section class="py-5 px-5 rounded-xl">
+                <div class="rounded-xl">
+                    <FeatureSection>
+                        <template #title>
+                            <h2>Nuestro catálogo</h2>
+                        </template>
+                        <template #content class="">
+                            <img src="/img/welcome/catalog-img.webp" alt="Imagen de una biblioteca" width="400px"
+                                height="300px" class="rounded-lg float-right ml-4 mb-4">
+                            <p>Disponemos de un amplio catálogo de libros. Accede a nuestro catálogo y encuentra el
+                                libro
+                                que necesitas para tu trabajo o simplemente algo para leer</p>
 
-                    </template>
-                    <template #footer>
-                        <div class="flex w-full justify-end">
-                            <PrimaryButton class="bg-cyan-600 hover:bg-cyan-400 focus:bg-cyan-700 active:bg-cyan-900">Ir
-                                a Catálogo</PrimaryButton>
-                        </div>
-                    </template>
-                </FeatureSection>
-                <FeatureSection>
-                    <template #title>
-                        <h2>Eventos</h2>
-                    </template>
-                    <template #content class="">
-                        <img src="/img/welcome/catalog-img.webp" alt="Imagen de una biblioteca" width="400px"
-                            height="300px" class="rounded-lg float-right ml-4 mb-4">
-                        <p>Disponemos de un amplio catálogo de libros. Accede a nuestro catálogo y encuentra el libro
-                            que necesitas para tu trabajo o simplemente algo para leer</p>
+                        </template>
+                        <template #footer>
+                            <div class="flex w-full justify-end">
+                                <PrimaryButton
+                                    class="bg-cyan-500 hover:bg-cyan-300 focus:bg-cyan-600 active:bg-cyan-800">Ir
+                                    a Catálogo</PrimaryButton>
+                            </div>
+                        </template>
+                    </FeatureSection>
+                    <FeatureSection>
+                        <template #title>
+                            <h2>Eventos</h2>
+                        </template>
+                        <template #content class="">
+                            <img src="/img/welcome/eventos.webp" alt="Imagen de una ponencia" width="400px"
+                                height="300px" class="rounded-lg float-right ml-4 mb-4">
+                            <p>Consulta nuestro calendario de eventos y actividades que diserrollamos dentro de nuestras instalaciones a lo largo del año.</p>
 
-                    </template>
-                    <template #footer>
-                        <div class="flex w-full justify-end">
-                            <PrimaryButton class="bg-cyan-600 hover:bg-cyan-400 focus:bg-cyan-700 active:bg-cyan-900">Ir
-                                a Eventos</PrimaryButton>
-                        </div>
-                    </template>
-                </FeatureSection>
-                <FeatureSection>
-                    <template #title>
-                        <h2>Comunidad</h2>
-                    </template>
-                    <template #content class="">
-                        <img src="/img/welcome/catalog-img.webp" alt="Imagen de una biblioteca" width="400px"
-                            height="300px" class="rounded-lg float-right ml-4 mb-4">
-                        <p>Disponemos de un amplio catálogo de libros. Accede a nuestro catálogo y encuentra el libro
-                            que necesitas para tu trabajo o simplemente algo para leer</p>
-                    </template>
-                    <template #footer>
-                        <div class="flex w-full justify-end">
-                            <PrimaryButton class="bg-cyan-600 hover:bg-cyan-400 focus:bg-cyan-700 active:bg-cyan-900">Ir
-                                a Comunidad</PrimaryButton>
-                        </div>
-                    </template>
-                </FeatureSection>
+                        </template>
+                        <template #footer>
+                            <div class="flex w-full justify-end">
+                                <PrimaryButton
+                                    class="bg-cyan-500 hover:bg-cyan-300 focus:bg-cyan-600 active:bg-cyan-800">Ir
+                                    a Eventos</PrimaryButton>
+                            </div>
+                        </template>
+                    </FeatureSection>
+                    <FeatureSection>
+                        <template #title>
+                            <h2>Comunidad</h2>
+                        </template>
+                        <template #content class="">
+                            <img src="/img/welcome/comunidad.webp" alt="Grupo de lectura" width="400px"
+                                height="300px" class="rounded-lg float-right ml-4 mb-4">
+                            <p>Accede a nuestra comunidad y encuentra personas con tus mismos intereses o recomendaciones de lectura.</p>
+                        </template>
+                        <template #footer>
+                            <div class="flex w-full justify-end">
+                                <PrimaryButton
+                                    class="bg-cyan-500 hover:bg-cyan-300 focus:bg-cyan-600 active:bg-cyan-800">Ir
+                                    a Comunidad</PrimaryButton>
+                            </div>
+                        </template>
+                    </FeatureSection>
+                </div>
+
             </section>
         </section>
 
