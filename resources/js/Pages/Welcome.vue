fd10cd6f2c2050ce1dc0304b5bbdc9d6ca2438c0
Fumetria <fulin789@gmail.com>
2025-05-22 00:27:46 +0200

    Update Welcome.vue to change button to link for catalog navigation and improve text formatting

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -40,9 +40,10 @@ const welcomeDescription = 'Accede a nuestro catálogo y encuentra el libro que 
                         </template>
                         <template #footer>
                             <div class="flex w-full justify-end">
-                                <button
-                                    class="inline-flex items-center px-4 py-2 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest bg-cyan-800 hover:bg-cyan-700 focus:bg-cyan-700 active:bg-cyan-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 transition ease-in-out duration-150">Ir
-                                    a Catálogo</button>
+                                <a :href="route('catalog')"><button
+                                        class="inline-flex items-center px-4 py-2 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest bg-cyan-800 hover:bg-cyan-700 focus:bg-cyan-700 active:bg-cyan-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 transition ease-in-out duration-150">Ir
+                                        a Catálogo</button></a>
+
                             </div>
                         </template>
                     </FeatureSection>
@@ -53,7 +54,8 @@ const welcomeDescription = 'Accede a nuestro catálogo y encuentra el libro que 
                         <template #content class="">
                             <img src="/img/welcome/eventos.webp" alt="Imagen de una ponencia" width="400px"
                                 height="300px" class="rounded-lg float-right ml-4 mb-4">
-                            <p>Consulta nuestro calendario de eventos y actividades que diserrollamos dentro de nuestras instalaciones a lo largo del año.</p>
+                            <p>Consulta nuestro calendario de eventos y actividades que diserrollamos dentro de nuestras
+                                instalaciones a lo largo del año.</p>
 
                         </template>
                         <template #footer>
@@ -69,9 +71,10 @@ const welcomeDescription = 'Accede a nuestro catálogo y encuentra el libro que 
                             <h2>Comunidad</h2>
                         </template>
                         <template #content class="">
-                            <img src="/img/welcome/comunidad.webp" alt="Grupo de lectura" width="400px"
-                                height="300px" class="rounded-lg float-right ml-4 mb-4">
-                            <p>Accede a nuestra comunidad y encuentra personas con tus mismos intereses o recomendaciones de lectura.</p>
+                            <img src="/img/welcome/comunidad.webp" alt="Grupo de lectura" width="400px" height="300px"
+                                class="rounded-lg float-right ml-4 mb-4">
+                            <p>Accede a nuestra comunidad y encuentra personas con tus mismos intereses o
+                                recomendaciones de lectura.</p>
                         </template>
                         <template #footer>
                             <div class="flex w-full justify-end">
