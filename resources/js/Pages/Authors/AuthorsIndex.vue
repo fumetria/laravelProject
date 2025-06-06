edb985ad2c4cd4b8dcc2e9de34d40082e31ca77d
Fumetria <fulin789@gmail.com>
2025-06-07 00:43:16 +0200

    Add new author button, table header and improve IU

--- resources/js/Pages/Authors/AuthorsIndex.vue
+++ resources/js/Pages/Authors/AuthorsIndex.vue
@@ -11,9 +11,15 @@ defineProps({
 
 <template>
     <AppLayout class="h-screen" title="Autores">
-        <section class="w-full flex justify-center items-center">
-            <table class="bg-slate-50">
-                <thead class="bg-emerald-400 text-white">
+        <section class="mx-auto w-full max-w-3xl flex flex-col justify-center items-center">
+            <div class="flex justify-between items-center w-full bg-emerald-600 px-4 py-2">
+                <h1 class="uppercase text-xl text-white">lista de autores</h1>
+                <button title="nuevo autor" class="bg-emerald-400 hover:bg-emerald-700 size-8 flex justify-center items-center rounded">
+                    <font-awesome-icon :icon="['fas', 'plus']" class="z-50 text-xl text-stone-50" />
+                </button>
+            </div>
+            <table class="bg-slate-50 w-full">
+                <thead class="bg-emerald-400  border-x border-collapse border-emerald-400 text-white">
                     <tr class="">
                         <th class="px-2 py-1 text-center">ID</th>
                         <th class="px-2 py-1 text-center">NOMBRE</th>
@@ -23,11 +29,11 @@ defineProps({
                 </thead>
                 <tbody>
                     <tr v-for="author in authors" :key="author.id">
-                        <td class="px-2 py-1 text-center">{{ author.id }}</td>
-                        <td class="px-2 py-1 text-center">{{ author.name }}</td>
-                        <td class="px-2 py-1 text-center">{{ author.created_at }}</td>
-                        <td class="px-2 py-1 text-center">
-                            <div class="flex gap-1 items-center">
+                        <td class="px-2 py-1 text-center border-x border-collapse border-emerald-400">{{ author.id }}</td>
+                        <td class="px-2 py-1 text-center border-x border-collapse border-emerald-400">{{ author.name }}</td>
+                        <td class="px-2 py-1 text-center border-x border-collapse border-emerald-400">{{ author.created_at }}</td>
+                        <td class="py-2 text-center flex justify-center items-center border-x border-collapse border-emerald-400">
+                            <div class="flex gap-3 items-center">
                                 <PrimaryButton>
                                     <font-awesome-icon :icon="['fas', 'eye']" class="z-50 text-l  text-stone-50" />
                                 </PrimaryButton>
@@ -42,6 +48,9 @@ defineProps({
                     </tr>
                 </tbody>
             </table>
+        </section>
+        <section>
+
         </section>
     </AppLayout>
 </template>
